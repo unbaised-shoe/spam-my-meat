3df2f37bf428159c5d07671794db53f6931d8407
base_shoe <71678940+unbaised-shoe@users.noreply.github.com>
2021-04-25 21:40:22 +1000

    Add files via upload

---
+++ spam_meat.py
@@ -0,0 +1,18 @@
+import pyautogui
+import time
+
+time.sleep(5)
+
+spam = open("meat")
+
+while True:
+    for word in spam:
+        pyautogui.typewrites 
+        pyautogui.press("enter")
+
+# 
+#
+#
+#
+
+
